e7ff369fbc7e4a49e0c6666dc7a7859a5e7bb40c
shootylife <a2x.lucas59@gmail.com>
2020-11-08 17:36:43 +0100

    feat: concessionary controller

--- src/main/java/com/example/CarSale/Concessionary/ConcessionaryController.java
+++ src/main/java/com/example/CarSale/Concessionary/ConcessionaryController.java
@@ -1,6 +1,7 @@
 package com.example.CarSale.Concessionary;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -17,8 +18,28 @@ public class ConcessionaryController {
         return concessionaryRepository.findAll();
     }
 
-    @GetMapping("/{concessionaryId}")
+    @GetMapping("search/{concessionaryId}")
     public Optional<Concessionary> getConcessionary(@PathVariable("concessionaryId") int concessionaryId){
         return concessionaryRepository.findById(concessionaryId);
+    }
+
+    @PostMapping("add")
+    public Concessionary addConcessionary(@RequestBody Concessionary concessionary){
+        return concessionaryRepository.save(concessionary);
+    }
+
+    @DeleteMapping("delete/{concessionaryId}")
+    public void deleteConcessionary(@PathVariable("concessionaryId") int concessionaryId){
+        concessionaryRepository.deleteById(concessionaryId);
+    }
+
+    @PutMapping("update/{concessionaryId}")
+    public ResponseEntity<Concessionary> updateConcessionary(@PathVariable("concessionaryId") int concessionaryId, @RequestBody Concessionary concessionaryDetails) throws Exception {
+        Concessionary concessionary = concessionaryRepository.findById(concessionaryId)
+                .orElseThrow(() -> new Exception("Concessionary not found for this id :: " + concessionaryId));
+
+        concessionary.setName(concessionaryDetails.getName());
+        final Concessionary updatedConcessionary = concessionaryRepository.save(concessionary);
+        return ResponseEntity.ok(updatedConcessionary);
     }
 }
