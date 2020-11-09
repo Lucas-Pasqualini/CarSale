3da83e19a4fafd7bca8ebc49e866187a8d374039
shootylife <a2x.lucas59@gmail.com>
2020-11-09 20:18:25 +0100

    refactor: add getter and setter

--- src/main/java/com/example/CarSale/Brand/Brand.java
+++ src/main/java/com/example/CarSale/Brand/Brand.java
@@ -2,19 +2,37 @@ package com.example.CarSale.Brand;
 
 import com.example.CarSale.Car.Car;
 import com.example.CarSale.Concessionary.Concessionary;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data
+@NoArgsConstructor
 public class Brand {
 
     @Id
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @OneToMany
     @JoinColumn(name = "brand_id")
@@ -26,5 +44,6 @@ public class Brand {
             joinColumns = @JoinColumn(name= "brand_id"),
             inverseJoinColumns = @JoinColumn(name = "concessionary_id")
     )
+    @JsonIgnoreProperties
     private Set<Concessionary> concessionaryList = new HashSet<>();
 }
--- src/main/java/com/example/CarSale/Concessionary/Concessionary.java
+++ src/main/java/com/example/CarSale/Concessionary/Concessionary.java
@@ -1,7 +1,7 @@
 package com.example.CarSale.Concessionary;
 
 import com.example.CarSale.Brand.Brand;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -11,14 +11,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data
 @NoArgsConstructor
 public class Concessionary {
 
     @Id
-    private String id;
+    private int id;
     private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @ManyToMany(mappedBy = "concessionaryList")
+    @JsonIgnoreProperties
     private Set<Brand> BrandList = new HashSet<>();
 }
